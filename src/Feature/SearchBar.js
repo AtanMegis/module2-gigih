3dde045f907e78e37fd3fcf9fd6b256d96cbe14c
Ignatius Samuel Megis <atanmegis.cn@gmail.com>
2022-03-22 11:57:46 +0700

    Homework Module2-Session1

---
+++ src/Feature/SearchBar.js
@@ -0,0 +1,12 @@
+import React, { Component }  from 'react';
+
+const SearchBar = () => {
+    return(
+        <form action="/search">
+        <input placeholder="Search" required/>
+        <button type="submit">Search</button>
+    
+        </form>
+        )
+};
+export default SearchBar;
---
+++ src/Feature/Song.js
@@ -0,0 +1,14 @@
+import React, { Component, useState }  from 'react';
+
+const ContainerSong = () => {
+    return( 
+       <div>
+       <label class="songTitle">Bohemian Rhapsody (The Original Soundtrack)</label>
+       <p class="songArtist">Queen</p>
+       <img src="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b"/>
+       </div>
+  
+    )
+}
+
+export default ContainerSong;
